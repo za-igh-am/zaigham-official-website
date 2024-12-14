1f95cd09354596f149ab6cf53520671360eede95
Zaigham <physzaigham@gmail.com>
2024-12-14 02:03:43 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)# app.py
+
+
